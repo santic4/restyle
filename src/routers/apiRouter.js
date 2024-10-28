20ccaf5f8a0531162f54f8d915917ea86e02ad96
Santiago Cañu <santisv4@gmail.com>
2024-10-28 01:10:07 -0300

    paypal-no

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -6,7 +6,6 @@ import { usersRouter } from './users/usersRouter.js'
 import { sessionRouter } from './users/sessionRouter.js'
 import { carritoRouter } from './carrito/carritoRouter.js'
 import { productsRouter } from './products/productsRouter.js'
-import { PayPalRouter } from './paymentsRouter/payPalRouter.js'
 import { MercadoPagoRouter } from './paymentsRouter/mercadoPagoRouter.js'
 import { categoryRouter } from './products/categoryRouter.js'
 import { CardsPay } from './paymentsRouter/cardsPay.js'
@@ -22,7 +21,6 @@ apiRouter.use('/session', sessionRouter)
 apiRouter.use('/productos', productsRouter)
 apiRouter.use('/admin', adminRouter)
 apiRouter.use('/carrito', carritoRouter)
-apiRouter.use('/paypal', PayPalRouter)
 apiRouter.use('/mercado-pago', MercadoPagoRouter)
 apiRouter.use('/categorias', categoryRouter)
 apiRouter.use('/tarjetas', CardsPay)
