aad4b6d72bc8a76e24ca67a445bc41e1af597fac
Santiago Cañu <santisv4@gmail.com>
2024-10-28 02:56:06 -0300

    cpd

--- src/app/app.js
+++ src/app/app.js
@@ -19,12 +19,12 @@ export const app = express();
 app.use(express.static(path.join('public', 'build')));
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://restyle-869o.onrender.com',
     credentials: true 
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://restyle-869o.onrender.com');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
  app.use((req, res, next) => {
     res.setHeader(
         "Content-Security-Policy",
-        "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; connect-src 'self' https://api.paypal.com; font-src 'self';"
+        "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self' data:; img-src 'self' data:; connect-src 'self' https://api.paypal.com https://api.mercadopago.com; font-src 'self' data:;"
     );
     next();
 });
