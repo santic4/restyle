66e6e2c57daac6404e0428b5b56b8ef3172beddb
Santiago Cañu <santisv4@gmail.com>
2024-10-30 19:53:24 -0300

    keyys

--- src/app/app.js
+++ src/app/app.js
@@ -19,12 +19,12 @@ export const app = express();
 app.use(express.static(path.join('public', 'build')));
 
  app.use(cors({
-     origin: 'http://localhost:3000',
+     origin: 'https://restyle-869o.onrender.com',
      credentials: true 
  }));
  
  app.use((req, res, next) => {
-     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+     res.header('Access-Control-Allow-Origin', 'https://restyle-869o.onrender.com');
      res.header('Access-Control-Allow-Credentials', 'true');
      next();
  });
